cb93332cecc620d18111e1f29a5c5f96ee8649eb
zzc <2714156899@zzc.cn>
2025-11-14 15:39:27 +0800

    ``` feat(dependency): 添加 jedis 依赖以支持 Redis 连接
    在 pom.xml 中引入了 jedis 客户端库，版本为 5.2.0，
    用于实现与 Redis 的连接和操作功能。
    同时在测试类中增加了对 jedis 的基础连接和读写测试。
    ```

--- src/test/java/ruiji/ruiji/RuijiApplicationTests.java
+++ src/test/java/ruiji/ruiji/RuijiApplicationTests.java
@@ -4,12 +4,31 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+
 @SpringBootTest
 @EnableTransactionManagement
+@Slf4j
 class RuijiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void testJedis(){
+		log.info("测试");
+		Jedis jedis = new Jedis("192.168.164.182",6379);
+
+		jedis.auth("123456");
+
+		log.info(jedis.ping());
+
+		jedis.set("dongbeiyujie","yujie");
+
+		log.info(jedis.get("dongbeiyujie"));
+
+		jedis.close();
 	}
 
 }
